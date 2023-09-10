f171149feceb2626f016f726756397c29028cf81
RodrigoF3rr <rodrigo.4dev@gmail.com>
2023-09-09 22:18:52 -0400

    feat: implement create user use case

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
+import { SharedModule } from './modules/shared/shared.module';
+import { UsersModule } from './modules/users/users.module';
 
 @Module({
-  imports: [],
-  controllers: [],
-  providers: [],
+  imports: [SharedModule, UsersModule],
 })
 export class AppModule {}

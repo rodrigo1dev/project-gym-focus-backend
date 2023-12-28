04631e4b9352b76f7eebb0988fd4665f5d35c649
RodrigoF3rr <rodrigo.4dev@gmail.com>
2023-12-27 22:34:43 -0400

    feat: add email validation check in EmailVerificationUseCase

--- src/modules/users/use-cases/email-verification.use-case.ts
+++ src/modules/users/use-cases/email-verification.use-case.ts
@@ -1,17 +1,24 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { MailService } from 'src/modules/mail/mail.service';
 import { emailVerificationDTO } from '../dtos/email-verification.dto';
 import { EmailValidationRepository } from '../repositories/email-validation.repository';
+import { FindUserByEmailRepository } from '../repositories/find-user-by-email.repository';
 @Injectable()
 export class EmailVerificationUseCase {
   constructor(
     private readonly mailService: MailService,
     private readonly emailValidationRepository: EmailValidationRepository,
+    private readonly findUserByEmailRepository: FindUserByEmailRepository,
   ) {}
 
   async execute(data: emailVerificationDTO): Promise<string> {
     const { email } = data;
 
+    const emailAlreadyInUse =
+      await this.findUserByEmailRepository.execute(email);
+
+    if (emailAlreadyInUse)
+      throw new BadRequestException('Email already in use');
     const randomCode = Math.random().toString(36).substring(7);
 
     await this.mailService.SendEmail({
